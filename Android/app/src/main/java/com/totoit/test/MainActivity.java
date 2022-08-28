a30a436f8fd0522de2a9859ee2fbad40f111447a
OobCrazy <gohelgo999@gmail.com>
2022-08-28 17:32:39 +0700

    pull to refresh webview

--- Android/app/src/main/java/com/totoit/test/MainActivity.java
+++ Android/app/src/main/java/com/totoit/test/MainActivity.java
@@ -1,6 +1,7 @@
 package com.totoit.test;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
@@ -10,23 +11,30 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    private SwipeRefreshLayout swipeContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initWebView();
+        initSwipeContainer();
     }
 
     @SuppressLint("SetJavaScriptEnabled")
     private void initWebView() {
-        WebView webView = findViewById(R.id.main_webview);
+        webView = (WebView) findViewById(R.id.main_webview);
         webView.setWebChromeClient(new WebChromeClient());
         webView.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 view.loadUrl(url);
                 return false;
+            }
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                swipeContainer.setRefreshing(false);
             }
         });
         WebSettings webSettings = webView.getSettings();
@@ -35,5 +43,13 @@ public class MainActivity extends AppCompatActivity {
         webSettings.setLoadWithOverviewMode(true);
         webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
         webView.loadUrl(getString(R.string.web_url));
+    }
+
+    private void initSwipeContainer() {
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipe_container);
+        swipeContainer.setOnRefreshListener(() -> {
+            webView.reload();
+        });
+
     }
 }
