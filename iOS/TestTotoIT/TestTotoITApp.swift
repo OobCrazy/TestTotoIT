1751ea26fa218f1998decfc8fffb7d88e6dd4dc3
OobCrazy <gohelgo999@gmail.com>
2022-08-30 02:20:16 +0700

    init ios

---
+++ iOS/TestTotoIT/TestTotoITApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTotoITApp.swift
+//  TestTotoIT
+//
+//  Created by Anucha Seetasung on 30/8/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct TestTotoITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
